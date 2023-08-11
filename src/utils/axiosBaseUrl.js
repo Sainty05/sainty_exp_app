25a69e2f9c8aae1da7427b19e454431fd6908025
Santosh Rathore <santosh.rathore@infraveo.com>
2023-08-11 12:08:43 +0530

    remove api frmo axios url

--- src/utils/axiosBaseUrl.js
+++ src/utils/axiosBaseUrl.js
@@ -1,5 +1,5 @@
 import Axios from 'axios';
 const axiosBaseURL = Axios.create({
-    baseURL: 'https://sainty-exp-server-05.vercel.app/api/'
+    baseURL: 'https://sainty-exp-server-05.vercel.app/'
 });
 export default axiosBaseURL
